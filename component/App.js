ac1c474cf7a73dd75a4d5e551b9bbd8c613fd121
YhinkaDevOps <yinkabakare30@gnail.com>
2022-06-22 00:51:16 +0100

    first react commit

---
+++ component/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Header from './component/Header'
+import './component/App.css'
+// import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
+
+
+
---
+++ component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../component/Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='header-class'>Basic react appplication</h1>
+    </div>
+  )
+}
+
+export default Header
